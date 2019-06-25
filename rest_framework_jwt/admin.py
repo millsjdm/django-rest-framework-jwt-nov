05b5de72b8d312145b0fe722c6c4ea1722f9ca8a
David Binetti <dbinetti@gmail.com>
2019-06-24 21:47:04 -0700

    Enable User admin

--- rest_framework_jwt/admin.py
+++ rest_framework_jwt/admin.py
@@ -14,7 +14,7 @@ from .models import User
 
 admin.site.site_header = 'Barberscore Admin Backend'
 
-# @admin.register(User)
+@admin.register(User)
 class UserAdmin(BaseUserAdmin):
     def has_add_permission(self, request):
         return False

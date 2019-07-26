b332b275f44d7a84ee7bdc0b64090410e4e38558
David Binetti <dbinetti@gmail.com>
2019-07-26 09:23:58 -0700

    Add id search filter

--- rest_framework_jwt/filtersets.py
+++ rest_framework_jwt/filtersets.py
@@ -7,6 +7,9 @@ class UserFilterset(FilterSet):
     class Meta:
         model = User
         fields = {
+            'id': [
+                'exact',
+            ],
             'username': [
                 'exact',
             ],

b7efc23fc40118ec030c9e273dcde4dc07df3cc6
David Binetti <dbinetti@gmail.com>
2019-07-26 18:31:04 -0700

    Make User admin Read-Only

--- rest_framework_jwt/admin.py
+++ rest_framework_jwt/admin.py
@@ -14,10 +14,23 @@ from .models import User
 
 admin.site.site_header = 'Admin Backend'
 
-# @admin.register(User)
+
+@admin.register(User)
 class UserAdmin(BaseUserAdmin):
     def has_add_permission(self, request):
         return False
+    def has_delete_permission(self, request, obj=None):
+        return False
+
+    def has_change_permission(self, request, obj=None):
+        return False
+
+    def changeform_view(self, request, object_id=None, form_url='', extra_context=None):
+        extra_context = extra_context or {}
+        extra_context['show_save_and_continue'] = False
+        extra_context['show_save'] = False
+        return super().changeform_view(request, object_id, extra_context=extra_context)
+
     form = UserChangeForm
     list_display = [
         'username',
